1601b87e644ae3db864073871300008ede1f12fd
siddu-asd <siddueee44@gmail.com>
2025-05-06 23:34:54 +0530

    latest

--- src/ViewerComponent.js
+++ src/ViewerComponent.js
@@ -1,37 +1,27 @@
 import React, { useEffect } from 'react';
 import { Viewer } from '@photo-sphere-viewer/core';
-import '@photo-sphere-viewer/core/index.css'; // Correct import for CSS
+import { AutorotatePlugin } from '@photo-sphere-viewer/autorotate-plugin';
+import '@photo-sphere-viewer/core/index.css';
 
 const ViewerComponent = () => {
   useEffect(() => {
-    // Initialize the PhotoSphereViewer once the component is mounted
     const viewer = new Viewer({
-      container: 'app-viewer-container', // The ID of the container where the viewer will be rendered
-      panorama: '/360-2.jpg', // Path to the 360-degree image (ensure this is correct)
-      navbar: false, // Disable the navbar (optional)
-      autoRotate: true, // Enables auto-rotation
-      autoRotateDelay: 5000, // Delay before auto-rotation starts (in ms)
-      autoRotateSpeed: 0.1, // Speed of auto-rotation (positive values rotate counter-clockwise, negative values clockwise)
+      container: 'app-viewer-container',
+      panorama: '/360-2.jpg',
+      navbar: false,
+      plugins: [
+        [AutorotatePlugin, { speed: '1rpm', delay: 8000 }]
+      ]
     });
 
-    // Log to ensure the viewer is initialized correctly
-    console.log(viewer);
-
-    // Add a hotspot at a specific position (latitude and longitude)
-    
-    // Clean up the viewer when the component is unmounted
-    return () => {
-      viewer.destroy();
-    };
+    return () => viewer.destroy();
   }, []);
 
   return (
-    <div 
-      id="app-viewer-container" 
-      style={{ width: '100%', height: '100vh',overflow:'hidden' }} // Ensure the viewer takes full viewport height
-    >
-      {/* This div will hold the 360-degree viewer */}
-    </div>
+    <div
+      id="app-viewer-container"
+      style={{ width: '100%', height: '100vh', overflow: 'hidden' }}
+    />
   );
 };
 
